eef80ea412329e2d855823beac7a683b1dadaa27
MHassaanButt <hassaanbutt67@gmail.com>
2021-07-01 20:19:31 +0500

    Final Appn

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,8 +2,13 @@ import Task from "./Task";
 const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
-      {tasks.map((task, index) => (
-        <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
       ))}
     </>
   );
